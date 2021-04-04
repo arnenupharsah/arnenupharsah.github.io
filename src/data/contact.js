792f574ed35ed9f43edc8b67550a7898e1a1c201
arnenupharsah <arnenupharsah@gmail.com>
2021-04-04 23:12:41 +0530

    Modifies

--- src/data/contact.js
+++ src/data/contact.js
@@ -5,7 +5,7 @@ import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
-import { faGraduationCap } from '@fortawesome/free-solid-svg-icons/faGraduationCap';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons/faGoogle';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 
@@ -48,8 +48,8 @@ const data = [
   {
     link: 'https://scholar.google.com/citations',
     label: 'Google Scholar',
-    icon: faGraduationCap,
-  },
+    icon: faGoogle,
+  }
 ];
 
 export default data;

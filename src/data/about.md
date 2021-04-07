fcb48cbd9b0375f5393c27bf0d9090feb7516ec5
Aulick Sah <aulick@amazon.com>
2021-04-07 17:46:52 +0530

    Updated Data

--- src/data/about.md
+++ src/data/about.md
@@ -14,10 +14,8 @@ Ask me in person for other stories that I'm afraid to share with the internet.
 
 - Running
 - Space
-- Summer
 - [Books](https://www.goodreads.com/aulicksah)
 - Podcasts ([The Daily](https://www.nytimes.com/column/the-daily), [Planet Money](https://www.npr.org/sections/money/), [The Indicator](https://www.npr.org/podcasts/510325/the-indicator-from-planet-money), [This American Life](https://www.thisamericanlife.org/), [99% Invisible](https://99percentinvisible.org/episodes/), [The Economist](http://radio.economist.com/), [Radiolab](https://www.wnycstudios.org/shows/radiolab), [Hidden Brain](https://www.npr.org/series/423302056/hidden-brain), [Inquiring Minds](https://inquiring.show), and others)
-- [Good design](/)=
 
 # Travel / Geography
 
--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -10,11 +10,11 @@ const positions = [
   },
   {
     company: 'Amazon',
-    position: 'Co-founder',
-    link: 'http://skepticalinvestments.biz',
-    daterange: 'April 2017 - Present',
+    position: 'SDE-Intern',
+    link: 'https://amazon.com',
+    daterange: 'Jan 2020 - Jun 2020',
     points: [
-      'As a part of the Communication Customer Experience (CCX) team, I worked on managing customerpreferences for communications sent to them via Email, SMS and Push. I have delivered on multiple projectsincluding E-mail templates, GDPR data compliance for customer’s communication preferences data, et',
+      'As a part of the Communication Customer Experience (CCX) team, I worked on managing customerpreferences for communications sent to them via Email, SMS and Push. I have delivered on multiple projectsincluding E-mail templates, GDPR data compliance for customer’s communication preferences data, etc.',
       'Framework/Tools/Programming Language Used: Java, Handlebars, Spring Framework, AWS Services(S3,DynamoDB, SQS, SNS, Lambdas and Redshift)',
     ],
   },
